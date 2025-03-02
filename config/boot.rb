73e46fc83dc88e9bff49a9152caa5bac71f959c1
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2025-03-02 22:10:41 +0530

    bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+if Rails.env.development? || Rails.env.test?
+  require 'bootsnap/setup'
+end
